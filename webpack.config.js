de8a5454db6c0f0ec3f9feb420e12436606c586f
kittykatchunks <james.frayne@gmail.com>
2026-01-05 19:27:10 +0000

    Updated code for Raspberry Pi deployment

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = (env, argv) => {
         './pwa/js/app-config.js',
         './pwa/js/language-manager.js',
         './pwa/js/browser-cache.js',
+        './pwa/js/tab-alert-manager.js',
         './pwa/js/sip-session-manager.js',
         './pwa/js/ui-state-manager.js',
         './pwa/js/busylight-manager.js',
