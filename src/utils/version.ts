fb9ea979371f2a5c4299df8a7eb3205afaa55ddb
kittykatchunks <james.frayne@gmail.com>
2026-01-23 00:17:33 +0000

    fix: ensure correct recording of missed calls in activity tab [v0.2.22-alpha.23]

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -16,7 +16,7 @@ function isVerboseLoggingEnabled(): boolean {
 
 // Import version from VERSION file (will be bundled)
 // This should be updated in VERSION file for each release
-const CURRENT_VERSION = '0.2.22-alpha.22'; // This should match VERSION file
+const CURRENT_VERSION = '0.2.22-alpha.23'; // This should match VERSION file
 
 // Storage key for last known version
 const LAST_VERSION_KEY = 'LastKnownVersion';
